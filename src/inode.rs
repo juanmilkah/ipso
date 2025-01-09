68db0624f2c3eeb7f010e6a8bc61b79f8eb8a836
Juan Milkah <juanmilkah@gmail.com>
2025-01-09 17:33:37 +0300

    add inode methods

--- src/inode.rs
+++ src/inode.rs
@@ -1,23 +1,158 @@
+use std::io::Result;
+
+#[derive(Default)]
 pub struct Inode {
-    pub identifier: u32,   /*inode number*/
-    pub mode: u32,         /*can this file be read/written/executed*/
-    pub ftype: FileType,   /*file type*/
-    pub uid: u32,          /*who owns this file*/
-    pub size: u64,         /*file size in bytes*/
-    pub time: u64,         /*last accessed*/
-    pub ctime: u64,        /*created at*/
-    pub mtime: u64,        /*modified at*/
-    pub dtime: u64,        /*time this inode is deleted*/
-    pub gid: u32,          /*owner group*/
-    pub dblocks: Vec<u32>, /*direct blocks*/
-    pub iblocks: Vec<u32>, /*indirect blocks*/
-    pub lcount: u32,       /*direct links count*/
+    pub identifier: u32,    /*inode number*/
+    pub mode: u32,          /*can this file be read/written/executed*/
+    pub ftype: FileType,    /*file type*/
+    pub uid: u32,           /*who owns this file*/
+    pub size: u64,          /*file size in bytes*/
+    pub time: u64,          /*last accessed*/
+    pub ctime: u64,         /*created at*/
+    pub mtime: u64,         /*modified at*/
+    pub dtime: u64,         /*time this inode is deleted*/
+    pub gid: u32,           /*owner group*/
+    pub dblocks: [u32; 12], /*direct blocks*/
+    pub iblocks: [u32; 3],  /*indirect blocks*/
+    pub lcount: u32,        /*direct links count*/
 }
 
+#[derive(Default, Clone)]
 pub enum FileType {
+    #[default]
     RegularFile,
     Directory,
     SymLink,
 }
 
-impl Inode {}
+impl Inode {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    pub fn is_valid(&self) -> bool {
+        self.size > 0 && self.identifier > 0 && self.mode > 0
+    }
+
+    pub fn get_identifier(&self) -> u32 {
+        self.identifier
+    }
+
+    pub fn set_identifier(&mut self, id: u32) {
+        self.identifier = id;
+    }
+
+    pub fn read_from_disk(&self) -> &[u8] {
+        &[]
+    }
+
+    pub fn write_to_disk(&mut self, _data: &[u8]) -> Result<()> {
+        Ok(())
+    }
+
+    pub fn get_file_type(&self) -> FileType {
+        self.ftype.clone()
+    }
+
+    pub fn set_file_type(&mut self, t: u32) {
+        let ftype = match t {
+            0 => Some(FileType::RegularFile),
+            1 => Some(FileType::Directory),
+            2 => Some(FileType::SymLink),
+            _ => None,
+        };
+
+        if let Some(t) = ftype {
+            self.ftype = t;
+        }
+    }
+
+    pub fn get_file_permissions(&self) -> u32 {
+        self.mode
+    }
+
+    pub fn set_file_permissions(&mut self, mode: u32) {
+        self.mode = mode;
+    }
+
+    pub fn get_owner(&self) -> u32 {
+        self.uid
+    }
+
+    pub fn set_owner(&mut self, id: u32) {
+        self.uid = id;
+    }
+
+    pub fn get_group(&self) -> u32 {
+        self.gid
+    }
+
+    pub fn set_group(&mut self, id: u32) {
+        self.gid = id;
+    }
+
+    pub fn get_size(&self) -> u64 {
+        self.size
+    }
+
+    pub fn set_size(&mut self, size: u64) {
+        self.size = size;
+    }
+
+    pub fn get_direct_blocks(&self) -> &[u32] {
+        &self.dblocks
+    }
+
+    pub fn set_direct_blocks(&mut self, blocks: [u32; 12]) {
+        self.dblocks.copy_from_slice(&blocks);
+    }
+
+    pub fn get_indirect_blocks(&self) -> &[u32] {
+        &self.iblocks
+    }
+
+    /*3 indirect blocks*/
+    pub fn set_indirect_blocks(&mut self, blocks: [u32; 3]) {
+        self.iblocks.copy_from_slice(&blocks);
+    }
+
+    pub fn get_creation_time(&self) -> u64 {
+        self.ctime
+    }
+
+    pub fn set_creation_time(&mut self, t: u64) {
+        self.ctime = t;
+    }
+
+    pub fn get_mod_time(&self) -> u64 {
+        self.mtime
+    }
+
+    pub fn set_mod_time(&mut self, t: u64) {
+        self.mtime = t;
+    }
+
+    pub fn get_links_count(&self) -> u32 {
+        self.lcount
+    }
+
+    pub fn set_links_count(&mut self, count: u32) {
+        self.lcount = count;
+    }
+
+    pub fn get_last_access(&self) -> u64 {
+        self.time
+    }
+
+    pub fn set_last_access(&mut self, time: u64) {
+        self.time = time;
+    }
+
+    pub fn get_deletion_time(&self) -> u64 {
+        self.dtime
+    }
+
+    pub fn set_deletion_time(&mut self, time: u64) {
+        self.dtime = time;
+    }
+}
